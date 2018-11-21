74c05f6c91e0ef3e730a40c00dcf4a1c00b4783d
Javier Zambra <jzambraf@gmail.com>
2018-11-21 16:17:29 -0300

    Fixes
    Visual fixes in searchOptions and remove of warnings in console related to code

--- src/components/ExploreData/exploreData.vue
+++ src/components/ExploreData/exploreData.vue
@@ -1,46 +1,45 @@
 <template>
-    <div>
-      <b-row>
-        <b-col cols="4">
-          <searchOptions :params.sync="params" :currentQueryParent.sync="query" :loading="loading"/>
-        </b-col>
-        <b-col cols="8">
-          <tabResult :loading.sync="loading" :params="params" :query_sql="query"/>
-        </b-col>
-          <loading
-            	:show="loading" label="Searching...">
-        	</loading>
-      </b-row>
+    <div class="container-fluid">
+        <b-row>
+            <b-col cols="4">
+                <searchOptions
+                    :params.sync="params"
+                    :currentQueryParent.sync="query"
+                    :loading="loading"
+                />
+            </b-col>
+            <b-col cols="8">
+                <tabResult :loading.sync="loading" :params="params" :query_sql="query"/>
+            </b-col>
+            <loading :show="loading" label="Searching..."></loading>
+        </b-row>
     </div>
 </template>
 
 <script>
 import searchOptions from "./searchOptions.vue";
 import tabResult from "./tabResult.vue";
-import loading from 'vue-full-loading'
+import loading from "vue-full-loading";
 export default {
-    name: 'exploreData',
-    props: {
-
-    },
-    components: {
-        searchOptions,
-        tabResult,
-        loading
-    },
-    data(){
-        return  {
-            query : "SELECT * FROM objects",
-            params : {},
-            loading : false,
-        }
-    },
-    methods: {
-        update(value) {
-            console.log(value);
-            this.params = value;
-        }
-
+  name: "exploreData",
+  props: {},
+  components: {
+    searchOptions,
+    tabResult,
+    loading
+  },
+  data() {
+    return {
+      query: "SELECT * FROM objects",
+      params: {},
+      loading: false
+    };
+  },
+  methods: {
+    update(value) {
+      console.log(value);
+      this.params = value;
     }
+  }
 };
 </script>
