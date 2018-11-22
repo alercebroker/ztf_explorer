2431fa19eac07ae20fbd79170d74fe7b09ae476d
Andrea Benavides <andrea.benavidesj@gmail.com>
2018-11-22 14:23:17 -0300

    layout scatter plot

--- src/components/ExploreData/tabScatter.vue
+++ src/components/ExploreData/tabScatter.vue
@@ -1,15 +1,86 @@
 <template>
 	<div class="tabScatter">
-		<h1>tabScatter</h1>
+		<b-row align-h="around"> 
+			<b-col cols="4"> 
+				<b-form-group horizontal label="xAxis" label-for="xAxis">
+					<b-form-select v-model="selectedX" :options="optionsX" id="xAxis"></b-form-select>
+				</b-form-group>
+			</b-col>
+			<b-col cols="4"> 
+				<b-form-group horizontal label="yAxis" label-for="yAxis">
+					<b-form-select v-model="selectedY" :options="optionsY" id="yAxis"></b-form-select>
+				</b-form-group>
+			</b-col>
+		</b-row>
+		<b-row align-h="center">
+			<div id="scatter" style="min-width: 310px; height: 400px; max-width: 800px; margin: 0 auto">
+				<p> Scatter not aviable</p>
+			</div>
+		</b-row>
 	</div>
 </template>
 
 <script>
 
 export default {
-  name: 'tabScatter',
-  props: {
-
-  }
+  	name: 'tabScatter',
+  	data(){
+		return {
+			selectedX: null,
+			selectedY: null,
+			optionsX: [
+				{ value: null, text: "Please select a variable" },
+				{ text: "Nr. Obs", value: "nobs" },
+				{
+					value: null,
+					text: "-- Magnitude  Band G --",
+					disabled: true
+				},
+				{ text: "Maxg", value: "maxg" },
+				{ text: "Ming", value: "Ming" },
+				{ text: "Meang", value: "Meang" },
+				{ text: "Slopeg", value: "Slopeg" },
+				{ text: "Firstmagg", value: "firstmagg" },
+				{ text: "Lastmagg", value: "lastmagg" },
+				{
+					value: null,
+					text: "-- Magnitude Band R --",
+					disabled: true
+				},
+				{ text: "Maxr", value: "maxr" },
+				{ text: "Minr", value: "Minr" },
+				{ text: "Meanr", value: "Meanr" },
+				{ text: "Sloper", value: "Sloper" },
+				{ text: "Firstmagr", value: "firstmagr" },
+				{ text: "Lastmagr", value: "lastmagr" },				
+			],
+			optionsY: [
+				{ value: null, text: "Please select a variable" },
+				{ text: "Nr. Obs", value: "nobs" },
+				{
+					value: null,
+					text: "-- Magnitude  Band G --",
+					disabled: true
+				},
+				{ text: "Maxg", value: "maxg" },
+				{ text: "Ming", value: "Ming" },
+				{ text: "Meang", value: "Meang" },
+				{ text: "Slopeg", value: "Slopeg" },
+				{ text: "Firstmagg", value: "firstmagg" },
+				{ text: "Lastmagg", value: "lastmagg" },
+				{
+					value: null,
+					text: "-- Magnitude Band R --",
+					disabled: true
+				},
+				{ text: "Maxr", value: "maxr" },
+				{ text: "Minr", value: "Minr" },
+				{ text: "Meanr", value: "Meanr" },
+				{ text: "Sloper", value: "Sloper" },
+				{ text: "Firstmagr", value: "firstmagr" },
+				{ text: "Lastmagr", value: "lastmagr" },				
+			]
+		}
+	}
 }
 </script>
