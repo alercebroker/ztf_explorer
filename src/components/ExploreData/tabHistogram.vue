145fc5245d6e97bf81f11a7a42330d9907e62e7a
Andrea Benavides <andrea.benavidesj@gmail.com>
2018-11-22 12:27:57 -0300

    yAxis selector

--- src/components/ExploreData/tabHistogram.vue
+++ src/components/ExploreData/tabHistogram.vue
@@ -1,15 +1,57 @@
 <template>
 	<div class="tabHistogram">
-		<h1>tabHistogram</h1>
+		<b-row> 
+			<b-col> 
+				<b-form-group horizontal label="yAxis" label-for="yAxis">
+					<b-form-select v-model="selected" :options="options" id="yAxis"></b-form-select>
+				</b-form-group>
+			</b-col>
+		</b-row>
 	</div>
 </template>
 
 <script>
 
 export default {
-  name: 'tabHistogram',
-  props: {
-
-  }
+  	name: 'tabHistogram',
+  	data(){
+		return {
+			selected: null,
+			options: [
+				{ value: null, text: "Please select a variable" },
+				{
+					value: null,
+					text: "-- Magnitude  Band G --",
+					disabled: true
+				},
+				{ text: "Maxg", value: "maxg" },
+				{ text: "Ming", value: "Ming" },
+				{ text: "Meang", value: "Meang" },
+				{ text: "Slopeg", value: "Slopeg" },
+				{ text: "Firstmagg", value: "firstmagg" },
+				{ text: "Lastmagg", value: "lastmagg" },
+				{
+					value: null,
+					text: "-- Magnitude Band R --",
+					disabled: true
+				},
+				{ text: "Maxr", value: "maxr" },
+				{ text: "Minr", value: "Minr" },
+				{ text: "Meanr", value: "Meanr" },
+				{ text: "Sloper", value: "Sloper" },
+				{ text: "Firstmagr", value: "firstmagr" },
+				{ text: "Lastmagr", value: "lastmagr" },
+				{
+					value: null,
+					text: "-- Dates --",
+					disabled: true
+				},
+				{ text: "FirstJd", value: "firstjd" },
+				{ text: "LastJd", value: "lastjd" },
+        		{ text: "DeltaJd", value: "deltajd" },
+				
+			]
+		}
+	}
 }
 </script>
