957a3b698f73fe9f80592de07af6a065b134270a
Andrea Benavides <andrea.benavidesj@gmail.com>
2018-11-27 10:29:37 -0300

    highcharts-vue module added, main.js config ok

--- src/components/ExploreData/scatter.vue
+++ src/components/ExploreData/scatter.vue
@@ -1,9 +1,20 @@
 <template>
 	<p>No Scatter plot yet</p>
+	
 </template>
 <script>
+	
 	export default {
         name: "scatter",
         props: ['data', 'xVariable', 'yVariable'],
+        data(){
+        	return{
+        		charOptions:{
+        			series:[{
+        				data:[1,2,3], //sample data
+        			}],
+        		},
+        	}
+        },
     }
 </script>
