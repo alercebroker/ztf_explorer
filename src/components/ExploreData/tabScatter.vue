0978a2723fb3f4418d3980a6743639ee43843d56
Andrea Benavides <andrea.benavidesj@gmail.com>
2018-11-29 15:21:19 -0300

    TODO test result picking and delete example data

--- src/components/ExploreData/tabScatter.vue
+++ src/components/ExploreData/tabScatter.vue
@@ -16,7 +16,7 @@
 			</b-col>
 		</b-row>
 		<b-row align-h="center">
-			<scatter :data="results" :plotValues="plotValues" :xVariable="selectedX" :yVariable="selectedY"></scatter>
+			<scatter :results="results" :plotValues="plotValues" :xVariable="selectedX" :yVariable="selectedY"></scatter>
 		</b-row>
 	</div>
 </template>
@@ -92,6 +92,59 @@ export default {
 				{oid: 3, pair:[15 , 3]},
 			],
 		}
+	},
+	methods:{
+		getAxisData: function(axis,obj){
+    		var x = axis.title.text;
+    		if (x == "Nr. Obs"){
+    			return obj.nobs;
+    		} if (x == "Maxg"){
+    			return obj.maxg;
+    		} if (x == "Ming"){
+    			return obj.ming;
+    		} if (x == "Meang"){
+    			return obj.meang;
+    		} if (x == "Slopeg"){
+    			return obj.slopeg;
+    		} if (x == "Firstmagg"){
+    			return obj.firstmagg;
+    		} if (x == "Lastmagg"){
+    			return obj.lastmagg;
+    		} if (x == "Maxr"){
+    			return obj.maxr;
+    		} if (x == "Minr"){
+    			return obj.minr;
+    		} if (x == "Meanr"){
+    			return obj.meanr;
+    		} if (x == "Sloper"){
+    			return obj.sloper;
+    		} if (x == "Firstmagr"){
+    			return obj.firstmagr;
+    		} if (x == "Lastmagr"){
+    			return obj.lastmagr;
+    		} else {
+    			return null;
+    		}
+    	},
+    	setPlotValues: function(){ // TODO testear con querys
+    		this.results.forEach(obj => { 
+    			this.plotValues.push({
+    				oid: obj.oid,
+    				pair:[
+    					getAxisData(this.selectedX, obj),
+    					getAxisData(this.selectedY, obj),
+    					],
+    			});
+    		});
+    	},
+	},
+	watch: {
+		selectedX: function(newVal, oldVal) { // watch it
+                this.setPlotValues();
+        },
+        selectedY: function(newVal, oldVal) { // watch it
+                this.setPlotValues();
+        },
 	},
 }
 </script>
