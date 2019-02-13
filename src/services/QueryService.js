d939757f6f5cc8ff258be92a2f904f8cdcea3c76
dirodriguezm <diegorodriguezmancini@gmail.com>
2019-02-13 10:23:45 -0300

    localhost api

--- src/services/QueryService.js
+++ src/services/QueryService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: "http://52.91.72.134:8084/v2",
+    baseURL: "http://localhost:8084/v2",
     withCredentials: false,
     headers: {
         Accept: "application/json",
