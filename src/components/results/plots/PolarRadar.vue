52e54667356ef858f9bef0ae3f4c86f6314afcd2
Javier <javier.arredondo.c@usach.cl>
2019-07-25 17:26:51 -0400

    ADD: polar radar by classifier

---
+++ src/components/results/plots/PolarRadar.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <v-chart :options="polar" autoresize></v-chart>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "polarradar",
+    props: ["probabilities", "classifier"],
+    data() {
+        return {
+            polar: {
+                title: {
+                    text: ""
+                },
+                tooltip:{
+                    formatter: function(params){
+                        var rowTable = (col1, col2) => "<tr> <td>" + col1 + "</td> <td>" + col2 + "</td> </tr>"
+                        let table = "<table> <tr> <th>Class</th> <th>(%)</th></tr>"
+                        for (var i = 0; i < params.value.length; i++) { table += rowTable(params.name[i], params.value[i]) }
+                        return table + "</table>"
+                    }
+                },
+                legend: {
+                    show: false
+                },
+                radar: {
+                    //shape: 'circle',
+                    splitNumber: 3,
+                    name: {
+                        textStyle: {
+                            color: '#fff',
+                            backgroundColor: '#999',
+                            borderRadius: 3,
+                            padding: [3, 5]
+                        }
+                    },
+                    indicator: [],
+                    backgroundColor: "red"
+                },
+                series: [
+                    {
+                        name: 'Probabilities (%)',
+                        type: 'radar',
+                        areaStyle: {normal: {}},
+                        data : []
+                    },
+                ]
+            }
+        };
+    },
+    mounted() {
+        this.getValues();
+        this.getXMATCH();
+    },
+    methods: {
+        getValues() {
+            var data = []
+            var names = []
+            var probs = this.probabilities
+            this.polar.radar.indicator =  Object.keys(probs)
+            .filter(function(x) {return x.endsWith("_prob")})
+            .map(function(x) {
+                data.push(probs[x])
+                names.push(x.split("_")[0])
+                return {name: x.split("_")[0], max: 1}
+            })
+            this.polar.series[0].data.push({value: data, name: names});
+        },
+    }
+};
+</script>
+
+<style scoped>
+.echarts {
+  margin: auto;
+  width: 100%;
+  height: 100%;
+  min-height: 350px;
+}
+</style>
