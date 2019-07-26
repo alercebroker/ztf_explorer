b39185a7434db09ffdf023bf2ebccac897f8f040
Camilo Valenzuela <cacke91@gmail.com>
2019-07-25 23:11:03 -0400

    Added compression

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
     configureWebpack: {
         plugins: [
-          new VuetifyLoaderPlugin()
+          new VuetifyLoaderPlugin(),
+          new CompressionWebpackPlugin()
         ],
       },
-    baseUrl: process.env.NODE_ENV === 'production' ?
+    publicPath: process.env.NODE_ENV === 'production' ?
         '/vue/' : '/'
 }
