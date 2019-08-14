7a4faa807e351ab703f372bab4c416992d6ab7df
Javier <javier.arredondo.c@usach.cl>
2019-08-14 16:28:29 -0400

    FIX: Issue #103. Click in logo and link to main page. Metadatas for google searches

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app-bar app dark color="header" clipped-left>
-    <v-img :src="require('../assets/logos/logoW250.png')" max-width="140px" max-height="98px" alt="Logo ALeRCE"></v-img>
+    <a href="http://alerce.science/" target="_blank">
+      <v-img :src="require('../assets/logos/logoW250.png')"  max-width="140px" max-height="98px" alt="Logo ALeRCE"></v-img>
+    </a>
     <v-spacer></v-spacer>
     <v-toolbar-items >
       <!-- <v-btn text>FAQ</v-btn> -->
