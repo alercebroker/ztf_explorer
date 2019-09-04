b286dc7a8e6cdd634799fadee732ce375f21f842
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2019-09-04 14:54:53 -0400

    removed footer

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
-  <v-app>
-    <Header/>
-    <v-content>
-        <router-view></router-view>
-    </v-content>
-  </v-app>
+    <v-app>
+        <Header />
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
 
 export default {
-  name: "App",
-  components: {
-    Header,
-    Footer,
-  }
+    name: "App",
+    components: {
+        Header,
+    },
+    
 };
 </script>
 
