4cef34cca3c48b7ad76037807a6d9976322f70b9
Camilo Valenzuela <cacke91@gmail.com>
2019-09-05 19:16:07 -0400

    Modified navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,60 @@
 <template>
   <v-app-bar app dark color="header" clipped-left>
-    <a href="http://alerce.science/" target="_blank">
-      <v-img :src="require('../assets/logos/logoW250.png')"  max-width="140px" max-height="98px" alt="Logo ALeRCE"></v-img>
-    </a>
+      <a :to="{'path':'/'}" >
+        <v-img :src="require('../assets/logos/footerAlerceWhite.png')"  max-width="40px" class="mr-4"></v-img>
+      </a>
+
+      <v-toolbar-title :to="{'path':'/'}" class="white--text mr-4">
+          <span>
+            ZTF Explorer
+          </span>
+        </v-toolbar-title>
+
+    <v-toolbar-items>
+      <v-btn
+      class="pl-5 pr-5 d-none d-md-flex text-capitalize"
+      v-for="item in items"
+      :href="item.link"
+      >{{ item.title }}</v-btn>
+    </v-toolbar-items>
+
+   <v-menu
+    full-width
+    :z-index="priority">
+     <template v-slot:activator="{ on }">
+          <v-app-bar-nav-icon class="d-xs-flex d-md-none" v-on="on"></v-app-bar-nav-icon>
+
+     </template>
+     <v-list>
+       <v-list-item
+         v-for="(item, index) in items"
+         :key="index"
+         :href="item.link"
+       >
+         <v-list-item-title>{{ item.title }}</v-list-item-title>
+       </v-list-item>
+     </v-list>
+   </v-menu>
+
     <v-spacer></v-spacer>
+
     <v-toolbar-items >
       <!-- <v-btn text>FAQ</v-btn> -->
     </v-toolbar-items>
   </v-app-bar>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      priority: 10,
+      items: [
+        { title: 'SN Hunter' , link:"http://snhunter.alerce.online"},
+        { title: 'About', link:"http://alerce.science" },
+        // { title: 'AGN Ranger' , link:"http://alerce.online"},
+        // { title: 'VS Scout' , link:"http://alerce.online"},
+
+      ],
+    }),
+  }
+</script>
