0c0590a3d3792955579e00f496e3f66ee745f508
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2019-09-06 10:26:14 -0400

    added information from other catalogs to aladin

--- src/components/results/cards/aladin.vue
+++ src/components/results/cards/aladin.vue
@@ -1,40 +1,91 @@
 <template>
-    <div id="aladin-lite-div"  />
+    <div id="aladin-lite-div" />
 </template>
 
 <script>
-    export default {
-        name: "aladin",
-        data(){
-            return {
-                aladin: null
-            }
-        },
-        mounted(){
-            this.aladin = A.aladin('#aladin-lite-div', {survey: "P/PanSTARRS/DR1/color-z-zg-g", fov:0.03, cooFrame: "J2000d"});
-            if(this.coordinates.meanRA && this.coordinates.meanDEC) this.aladin.gotoRaDec(this.coordinates.meanRA, this.coordinates.meanDEC)
-        },
-        watch:{
-            coordinates(newCoord){
-                if(this.aladin)this.aladin.gotoRaDec(newCoord.meanRA, newCoord.meanDEC)
-            }
-        },
-        computed: {
-            coordinates(){
-                return {
-                    meanRA: this.$store.state.results.selectedObject ? this.$store.state.results.selectedObject.meanra : null,
-                    meanDEC: this.$store.state.results.selectedObject ? this.$store.state.results.selectedObject.meandec : null
-                }
+export default {
+    name: "aladin",
+    data() {
+        return {
+            aladin: null
+        };
+    },
+    methods: {
+        callback(data) {
+            console.log("CALLBACK", data);
+        }
+    },
+    mounted() {
+        this.aladin = A.aladin("#aladin-lite-div", {
+            survey: "P/PanSTARRS/DR1/color-z-zg-g",
+            fov: 0.03,
+            cooFrame: "J2000d"
+        });
+        if (this.coordinates.ra && this.coordinates.dec) {
+            this.aladin.gotoRaDec(this.coordinates.ra, this.coordinates.dec);
+            this.aladin.addCatalog(
+                A.catalogFromSimbad(this.coordinates, 0.1, {
+                    onClick: "showTable"
+                })
+            );
+            this.aladin.addCatalog(
+                A.catalogFromNED(this.coordinates, 0.1, {
+                    onClick: "showTable",
+                    shape: "plus"
+                })
+            );
+            this.aladin.addCatalog(
+                A.catalogFromVizieR("I/311/hip2", this.coordinates, 5, {
+                    onClick: "showTable"
+                })
+            );
+        }
+    },
+    watch: {
+        coordinates(newCoord) {
+            if (this.aladin) {
+                this.aladin.gotoRaDec(
+                    this.coordinates.ra,
+                    this.coordinates.dec
+                );
+                this.aladin.addCatalog(
+                    A.catalogFromSimbad(this.coordinates, 0.1, {
+                        onClick: "showTable"
+                    })
+                );
+                this.aladin.addCatalog(
+                    A.catalogFromNED(this.coordinates, 0.1, {
+                        onClick: "showTable",
+                        shape: "plus"
+                    })
+                );
+                this.aladin.addCatalog(
+                    A.catalogFromVizieR("I/311/hip2", this.coordinates, 0.1, {
+                        onClick: "showTable"
+                    })
+                );
             }
         }
+    },
+    computed: {
+        coordinates() {
+            return {
+                ra: this.$store.state.results.selectedObject
+                    ? this.$store.state.results.selectedObject.meanra
+                    : null,
+                dec: this.$store.state.results.selectedObject
+                    ? this.$store.state.results.selectedObject.meandec
+                    : null
+            };
+        }
     }
+};
 </script>
 
 <style scoped>
-#aladin-lite-div{
-  height:"100%";
-  width:"100%";
-  min-height: 200px;
-
+#aladin-lite-div {
+    height: "100%";
+    width: "100%";
+    min-height: 200px;
 }
 </style>
