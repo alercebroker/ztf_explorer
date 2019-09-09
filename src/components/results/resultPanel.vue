e854152582218761b3615879a4712000e6cc0e83
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2019-09-09 15:10:50 -0300

    no tabs

--- src/components/results/resultPanel.vue
+++ src/components/results/resultPanel.vue
@@ -1,36 +1,39 @@
 <template>
-  <div>
-        <v-tabs v-if="$vuetify.breakpoint.smAndDown" dark background-color="toolbar" :value="selectedTab">
-          <v-tab>Search</v-tab>
-          <v-tab-item v-if="$vuetify.breakpoint.smAndDown">
-            <v-layout row wrap>
-              <v-flex xs10 offset-xs1 >
-                <search-options @onSearch="onSearch"></search-options>
-              </v-flex>
-            </v-layout>
-          </v-tab-item>
+    <div>
+        <v-tabs
+            v-if="$vuetify.breakpoint.smAndDown"
+            dark
+            background-color="toolbar"
+            :value="selectedTab"
+        >
+            <v-tab>Search</v-tab>
+            <v-tab-item v-if="$vuetify.breakpoint.smAndDown">
+                <v-layout row wrap>
+                    <v-flex xs10 offset-xs1>
+                        <search-options @onSearch="onSearch"></search-options>
+                    </v-flex>
+                </v-layout>
+            </v-tab-item>
             <v-tab>Results</v-tab>
             <v-tab-item>
                 <tab-data />
             </v-tab-item>
         </v-tabs>
-        <div v-else="$vuetify.breakpoint.smAndDown" >
-          <v-card width="100%" class="ma-2">
-            <v-card-title primary-title>
-              Results
-            </v-card-title>
-            <v-divider></v-divider>
-            <v-card-text>
-              <tab-data />
-            </v-card-text>
-          </v-card>
+        <div v-else>
+            <v-card width="100%" class="ma-2">
+                <v-card-title primary-title>Results</v-card-title>
+                <v-divider></v-divider>
+                <v-card-text>
+                    <tab-data />
+                </v-card-text>
+            </v-card>
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
 import tabData from "./tabData.vue";
-import overviewCards from "./cards/overviewCards.vue"
+import overviewCards from "./cards/overviewCards.vue";
 import searchOptions from "@/components/search/searchOptions.vue";
 
 export default {
@@ -41,11 +44,11 @@ export default {
         overviewCards
     },
     methods: {
-      onSearch() {
-        if(this.$vuetify.breakpoint.smAndDown){
-          this.selectedTab = 1;
+        onSearch() {
+            if (this.$vuetify.breakpoint.smAndDown) {
+                this.selectedTab = 1;
+            }
         }
-      },
     },
     computed: {
         selectedTab: {
