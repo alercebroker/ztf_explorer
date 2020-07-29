35a5e036cba49112f11b08be1f72e260f0ee37b3
Diego <diego@pop-os.localdomain>
2020-07-29 18:17:14 -0400

    remove unnecesary elements from template

--- pages/object/_oid.vue
+++ pages/object/_oid.vue
@@ -1,85 +1,77 @@
 <template>
-  <!--div
-    tabindex="0"
-    @keydown.right="changeObject(1)"
-    @keydown.left="changeObject(-1)"
-    id="#details"
-  -->
-  <div>
-    <v-container fluid>
-      <v-row align="stretch">
-        <card-basic-information
-          :information="objectInformation"
-          :tns="tns"
-          :show="objectInformation != null"
-          card-class="grid-card"
-          lg="3"
-          md="6"
-          sm="12"
-        />
+  <v-container fluid>
+    <v-row align="stretch">
+      <card-basic-information
+        :information="objectInformation"
+        :tns="tns"
+        :show="objectInformation != null"
+        card-class="grid-card"
+        lg="3"
+        md="6"
+        sm="12"
+      />
 
-        <card-light-curve
-          :lightcurve="objectLightcurve"
-          :period="0.5"
-          :show="objectLightcurve.loaded"
-          card-class="grid-card"
-          lg="6"
-          md="6"
-          sm="12"
-        />
+      <card-light-curve
+        :lightcurve="objectLightcurve"
+        :period="0.5"
+        :show="objectLightcurve.loaded"
+        card-class="grid-card"
+        lg="6"
+        md="6"
+        sm="12"
+      />
 
-        <!--card-aladin
-          v-model="selectedObject"
-          :objects="objects"
-          :object-data="objectInformation"
-          :show="objectInformation != null"
-          card-class="grid-card"
-          lg="3"
-          md="6"
-          sm="12"
-        /-->
+      <card-aladin
+        v-model="selectedObject"
+        :objects="objects"
+        :object-data="objectInformation"
+        :show="objectInformation != null"
+        :card-class="grid - card"
+        lg="3"
+        md="6"
+        sm="12"
+      />
 
-        <card-mag-stats
-          :stats="stats"
-          :show="objectInformation != null"
-          card-class="grid-card"
-          lg="3"
-          md="6"
-          sm="12"
-        />
+      <card-mag-stats
+        :stats="stats"
+        :show="objectInformation != null"
+        card-class="grid-card"
+        lg="3"
+        md="6"
+        sm="12"
+      />
 
-        <card-classifiers
-          :classifiers="objectClassification.classifiers"
-          :show="objectClassification.loaded"
-          card-class="grid-card"
-          cols="12"
-          lg="3"
-          md="6"
-          sm="12"
-        />
+      <card-classifiers
+        :classifiers="objectClassification.classifiers"
+        :show="objectClassification.loaded"
+        card-class="grid-card"
+        cols="12"
+        lg="3"
+        md="6"
+        sm="12"
+      />
 
-        <card-stamps
-          v-model="selectedDetection"
-          :show="objectInformation != null"
-          :detections="objectLightcurve.detections"
-          :oid="selectedObject"
-          :cross-hair-space="25"
-          card-class="grid-card"
-          lg="6"
-          md="6"
-        />
+      <card-stamps
+        v-model="selectedDetection"
+        :show="objectInformation != null"
+        :detections="objectLightcurve.detections"
+        :oid="selectedObject"
+        :cross-hair-space="25"
+        card-class="grid-card"
+        lg="6"
+        md="6"
+      />
 
-        <card-cross-matches
-          :data="crossmatches.data"
-          :show="crossmatches.loaded"
-          cols="12"
-          lg="12"
-          md="12"
-          sm="12"
-        />
-      </v-row>
-    </v-container>
-  </div>
+      <card-cross-matches
+        :data="crossmatches.data"
+        :show="crossmatches.loaded"
+        cols="12"
+        lg="12"
+        md="12"
+        sm="12"
+      />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
