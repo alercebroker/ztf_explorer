d235d228d6d029d25e7e391aa04da736bf3b0879
Javier <javier.arredondo.c@usach.cl>
2021-03-17 10:02:34 -0300

    fix nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
     '@nuxtjs/vuetify',
-    'component-library/nuxt',
+    '@alerce/components/nuxt',
     ['@nuxtjs/dotenv', { filename: `.env.${process.env.NODE_ENV}` }],
   ],
   /*
