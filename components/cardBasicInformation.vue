79de30b3e97257c2a66ef0d6d6dded2dd2914ea7
Javier <javier.arredondo.c@usach.cl>
2021-10-07 09:27:13 -0300

    CHG: Button text changed. 'External archives' to 'Other archives'

--- components/cardBasicInformation.vue
+++ components/cardBasicInformation.vue
@@ -27,7 +27,7 @@
             <buttons-catalogs-buttons
               :ra="information.meanra"
               :dec="information.meandec"
-              title="External archives"
+              title="Other archives"
             />
           </v-col>
         </v-row>
