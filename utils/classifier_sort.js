06a708add6b638f3bf96e39faa26d248c444c03a
hectorxh <hectorplj@gmail.com>
2024-08-27 14:50:08 +0000

    feat: update classifiers priority list

--- utils/classifier_sort.js
+++ utils/classifier_sort.js
@@ -1,11 +1,12 @@
 const priorities = {
-  lc_classifier: 1,
-  lc_classifier_top: 2,
-  stamp_classifier: 3,
+  lc_classifier: 0,
+  lc_classifier_top: 1,
+  stamp_classifier: 2,
+  'LC_classifier_ATAT_forced_phot(beta)': 3,
+  'LC_classifier_BHRF_forced_phot(beta)': 4,
 }
-
 export function filterSortClassifiers(classifiers) {
   return classifiers
     .filter((classifier) => classifier.name in priorities)
-    .sort((a, b) => priorities[a] - priorities[b])
+    .sort((a, b) => priorities[a.name] - priorities[b.name])
 }
