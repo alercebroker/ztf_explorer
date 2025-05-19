15c879617ddd8d574f0968d0136a74d8e6a65dba
Demurest <eric.apolonio@hotmail.com>
2025-05-19 10:56:58 -0400

    new stamp classifier beta

--- utils/classifier_sort.js
+++ utils/classifier_sort.js
@@ -4,6 +4,7 @@ const priorities = {
   stamp_classifier: 2,
   'LC_classifier_ATAT_forced_phot(beta)': 3,
   'LC_classifier_BHRF_forced_phot(beta)': 4,
+  stamp_classifier_2025_beta: 5,
 }
 export function filterSortClassifiers(classifiers) {
   return classifiers
