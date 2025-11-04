4e95a886d54f6ff6a6ec0e702dc42be22111dacf
Demurest <eric.apolonio@hotmail.com>
2025-11-04 10:29:30 -0300

    connect to staging

--- components/cardBasicInformation.vue
+++ components/cardBasicInformation.vue
@@ -74,13 +74,8 @@ export default class CardObject extends Vue {
   }
 
   _loadHtmx(objectId, params) {
-    // const url = new URL(
-    //   `object_api/htmx/object_information?oid=${objectId}&survey_id=${params.survey}`,
-    //   this.$config.alerceApiBaseUrl
-    // )
-
     const url = new URL(
-      `http://127.0.0.1:8000/htmx/object_information?oid=3245230435995&survey_id=lsst`,
+      `object_api/htmx/object_information?oid=${objectId}&survey_id=${params.survey}`,
       this.$config.alerceApiBaseUrl
     )
 
--- components/sideBar.vue
+++ components/sideBar.vue
@@ -63,12 +63,10 @@ export default class sideListWrapper extends Vue {
   }
 
   _loadHtmx() {
-    // const url = new URL(
-    //   'object_api/htmx/side_objects',
-    //   this.$config.alerceApiBaseUrl
-    // )
-
-    const url = new URL(`http://127.0.0.1:8000/htmx/side_objects`)
+    const url = new URL(
+      'object_api/htmx/side_objects',
+      this.$config.alerceApiBaseUrl
+    )
 
     for (const [key, value] of Object.entries(this.QueryParams)) {
       url.searchParams.append(key, value)
