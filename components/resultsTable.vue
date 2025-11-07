c5833a22fd2af3e4a47bd74e916bd91481c4dcad
Demurest <eric.apolonio@hotmail.com>
2025-11-07 12:29:09 -0300

    table url global

--- components/resultsTable.vue
+++ components/resultsTable.vue
@@ -91,12 +91,7 @@ export default class ResultTableWrapper extends Vue {
   }
 
   _loadHtmx() {
-    // const url = new URL(
-    //   'object_api/htmx/list_objects',
-    //   this.$config.alerceApiBaseUrl
-    // )
-
-    const url = new URL('http://127.0.0.1:8000/htmx/list_objects')
+    const url = new URL('htmx/list_objects', this.$config.objectApiBaseUrl)
 
     for (const [key, value] of Object.entries(this.QueryParams)) {
       url.searchParams.append(key, value)
