25bfe79c8d0699c829efaadcaa896cae4b7d2e9a
Demurest <eric.apolonio@hotmail.com>
2025-11-07 14:37:15 -0300

    sidebar global url

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,7 +93,7 @@ export default {
    */
   publicRuntimeConfig: {
     objectApiBaseUrl:
-      process.env.OBJECT_API_BASE_URL || 'http://127.0.0.1:8000',
+      process.env.OBJECT_API_BASE_URL || 'http://127.0.0.1:8000/',
     avroApiBaseUrl:
       process.env.AVRO_API_BASE_URL || 'https://avro.alerce.online',
     catshtmApiBaseUrl:
