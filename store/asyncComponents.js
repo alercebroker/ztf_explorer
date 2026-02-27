d14647c66319cba99dec682231f1f70693d0a3d0
Claudio Mansilla <c.mansillabrito@gmail.com>
2026-02-27 17:20:25 -0300

    added mutations to control de oids changes

--- store/asyncComponents.js
+++ store/asyncComponents.js
@@ -8,14 +8,36 @@ import {
 @Module({ name: 'asyncComponents', namespaced: true, stateFactory: true })
 export default class ComponentsStore extends VuexModule {
   sideBarLoaded = false
+  globalLoading = false
+  loadingOid = ''
 
   @VuexMutation
   setSideBarLoad(val) {
     this.sideBarLoaded = val
   }
 
+  @VuexMutation
+  setGlobalLoading(val) {
+    this.globalLoading = val
+  }
+
+  @VuexMutation
+  setLoadingOid(val) {
+    this.loadingOid = val
+  }
+
   @VuexAction({ rawError: true })
   setSideBarLoadingAction(val) {
     this.setSideBarLoad(val)
+  }
+
+  @VuexAction({ rawError: true })
+  setGlobalLoadingAction(val) {
+    this.setGlobalLoading(val)
+  }
+
+  @VuexAction({ rawError: true })
+  setLoadingOidAction(val) {
+    this.setLoadingOid(val)
   }
 }
